0e0cd254cc3457cd60e7c6c17afbaa0b7558e4cf
Ruly Hajar Fauziah <rulyhajarfauziah@gmail.com>
2024-10-22 09:34:32 +0800

    Update matriks3x3.py

--- matriks3x3.py
+++ matriks3x3.py
@@ -1,51 +1,39 @@
-baris_A=int(input("Masukkan ukuran baris matriks A= ")) kolom_A=int(input("Masukkan ukuran kolom matriks A= "))
 
-baris_B-int(input("Masukkan ukuran baris matriks B- "))
+baris_A=int(input("masukkan ukuran baris matriks A= "))
+kolom_A=int(input("masukkan ukuran kolom matriks A= "))
 
-kolom_B=int(input("Masukkan ukuran kolom matriks B- "))
+baris_B=int(input("masukkan ukuran baris matriks B= "))
+kolom_B=int(input("masukkan ukuran kolom matriks B= "))
 
-if (kolom_A!-baris_B):
-
-print ("Matriks Tidak Dapat Dikalikan!")
-
+if (kolom_A!=baris_B):
+    print ("matriks tidak dapat dikalikan!")
 else:
-
-print ("Masukkan element matriks A : ") A=[]
-
-for i in range (baris_A):
-
-baris=[]
-
-for j in range (kolom_A):
-
-value-int(input("Masukkan element: "))
-
-baris.append(value)
-
-A.append(baris)
-
-print("Masukkan element matriks B") B=[]
-
+    print ("masukkan elemen matriks A : ")
+    A = []
+    for i in range (baris_A):
+        baris=[]
+        for j in range (kolom_A):
+            value = int(input("elemen matriks A: "))
+            baris.append(value)
+            A.append(baris)
+            
+print("masukkan elemen matriks B ")
+B = []
 for i in range (baris_B):
+    baris=[]
+    for j in range (kolom_B):
+        value = int(input("elemen matriks B: "))
+        baris.append(value)
+        B.append(baris)
 
-baris=[]
+    print ("matriks A : ", A)
+    print ("matriks B : ", B)
 
-for j in range (kolom_B):
 
-value-int(input("Masukkan element: "))
-
-baris.append(value)
-
-B.append(baris)
-
-print ("Matriks A: ", A)
-
-print ("Matriks B: ", B)
-
-hasil_kali [[0 for in range(kolom_B)] for in range(baris_A)]
-
+hasil_kali = [[0 for _ in range(kolom_B)] for _ in range(baris_A)]
 for i in range(baris_A):
+    for j in range(kolom_B):
+        for k in range(kolom_A):
+            hasil_kali[i][j]+=A[i][k]*B[k][j]
 
-for j in range(kolom_B): for k in range(kolom_A):
-
-hasil kali[i][1]+=A[i][k]*B[k][11
+print ("hasil perkalian matriks A dengan B adalah ", hasil_kali)
